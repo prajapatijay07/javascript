3a9e3f69bad5c080f2bf9f416986a5bf97f443ac
Jay Prajapati <prajapatijay0729@gmail.com>
2025-03-18 16:55:49 +0530

    loop

---
+++ loop/js/loop.js
@@ -0,0 +1,24 @@
+
+
+// While loop
+
+// let i = 1;
+// while (i <= 15) {
+//     console.log(i ,"Hello");
+//     i++;
+// }
+
+// Do while loop
+
+// let i = 1;
+
+//  do{
+//     console.log(i ,"Hello");
+//     i++;
+// } while (i <= 15)
+
+// For loop
+
+for(let i = 1; i <= 10; i++){
+    console.log(i ,"Hello");    
+}
