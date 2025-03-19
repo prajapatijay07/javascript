70d459f8580b0d9c17b50a89c25cdc2d76a885cf
Jay Prajapati <prajapatijay0729@gmail.com>
2025-03-19 10:53:30 +0530

    variable

---
+++ variable/js/variable.js
@@ -0,0 +1,33 @@
+
+// variables 3 types => var, let, const
+
+// var => function scope => re-declare => re-assign => last value print 
+
+var a = 10;
+
+var a = 15;
+
+console.log(a);
+
+
+
+// let => block scope => re-declare not possible => re-assign possible => last value print
+
+let b = 20;
+
+b = 25;
+
+console.log(b);
+
+
+
+// const => block scope => re-declare not possible => re-assign not possible => last value print
+
+const d = 40;
+
+const c = 45;
+
+console.log(d);
+
+console.log(c);
+
