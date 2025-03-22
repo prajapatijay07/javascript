caaeadf0a239cc710694c056af6ae2e48249f406
Jay Prajapati <prajapatijay0729@gmail.com>
2025-03-22 20:08:17 +0530

    operators

---
+++ operators/js/operators.js
@@ -0,0 +1,76 @@
+
+// Arithmetic operators (+, -, *, /, %, **, ++, --)
+
+let x = 2;
+let y = 3;
+
+// console.log(x + y); // 5
+// console.log(x - y); // -1
+// console.log(x * y); // 6
+// console.log(x / y); // 0.6666666666666666
+// console.log(x % y); // 2
+// console.log(x ** y); // 8
+// console.log(x++); // 2
+// console.log(x--); // 3
+// console.log(++x); // 3
+// console.log(--x); // 2
+
+// Assignment operators (=, +=, -=, *=, /=, %=, **=)
+
+let a = 25;
+
+// console.log(a = 25);
+// console.log(a += 25); // 50
+// console.log(a -=25); //0
+// console.log(a *= 25); //625
+// console.log(a /= 25); // 1
+// console.log(a %= 25); //0
+// console.log(a **= 25); // 8.88
+
+
+// Comparison operators (== , === , != , !== , > , < , >= , <=)
+
+// let b = 50;
+
+// console.log(b == 50); // true
+// console.log(b === "50"); // false
+// console.log(b != 20); // true (! => it mins "not")
+// console.log(b !== 50); // false
+// console.log(b < 55); // true
+// console.log(b > 55); // false
+// console.log(b >= 49); // true
+// console.log(b <= 49); // false
+
+
+// Logical Opertators (&& , // , !)
+
+// [&& => bane true hoy to true , bane false hoy to false , ak true ak false hoy to false]
+
+// console.log(5 < 6 && 7 < 5); // false
+// console.log(5 < 6 && 6 > 5); // true
+// console.log(5 < 4 && 6 < 5); // false
+// console.log(5 < 6 || 7 < 5); // true
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
