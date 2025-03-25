e3b8427e63d298c736db5784e4179d35c2aeb1c4
Jay Prajapati <prajapatijay0729@gmail.com>
2025-03-25 15:10:36 +0530

    funstion

---
+++ funstion/js/funstion.js
@@ -0,0 +1,68 @@
+
+
+// function maxnumber(){
+    // let a , b
+    //  a  = prompt("enter your number a")
+    //  b = prompt("enter your number b")
+    // if (a > b) {
+    //     console.log("A is a big number");
+    // }else{
+    //     console.log("B is a big number");
+        
+    // }
+// }
+
+
+// function minnumber(){
+    // let a = 20 , b = 25
+//     if (a < b) {
+//         console.log("A is a smole number");
+//     }else{
+//         console.log("B is a smole number");
+        
+//     }
+// // }
+
+    // let a = 10, b = 15
+    // a > b ? console.log("a is a biggest number") : console.log("b is a biggest number")
+    
+
+// maxnumber()
+// minnumber()
+
+// Parameters Function (with Arguments)
+// Return type in function
+// Switch Program
+
+// 4 types = NANR, NAWR, TANR, TAWR 
+
+// function add(){
+//     let a = 15 , b = 10;
+//     let total = a + b 
+//     console.log("Valayu off total", total);
+// }
+
+// add()
+
+// function add(){
+//     let a = 25 , b = 50;
+//     let total = a + b
+//     return total
+// }
+
+// console.log(add());
+
+// function add(a, b, c) {
+//     let totle = a * b * c 
+//     console.log(totle);
+// }
+
+// add(5, 2, 5)
+
+// function add(a, b){
+//     let totle = a / b
+//     return totle
+// }
+// console.log(add(15, 5));
+
+
